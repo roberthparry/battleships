6bb45db0641fff2fde9565156da8e5069e1ef28d
roberthparry <robert.h.parry@gmail.com>
2018-10-07 14:58:49 +0100

    write tests for cell reference translation

--- BattleShipsService/Cell.cs
+++ BattleShipsService/Cell.cs
@@ -3,13 +3,13 @@ namespace BattleShipsService
     internal class Cell
     {
         /// <summary>
-        /// The ship at the target.
+        /// The ship at the cell.
         /// </summary>
         /// <value></value>
         public Ship Ship { get; set; }
 
         /// <summary>
-        /// Status of ship at target (Intact, Damaged)
+        /// Status of ship at cell (Intact, Damaged)
         /// </summary>
         public CellStatus Status { get; set; }
     }
